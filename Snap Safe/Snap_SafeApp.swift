8e1011c7d8eb3061f5cb3ecb97b12061781b930c
Bill Booth <bill@erke.io>
2025-05-02 23:46:32 -0700

    Initial Commit

---
+++ Snap Safe/Snap_SafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_SafeApp.swift
+//  Snap Safe
+//
+//  Created by Bill Booth on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Snap_SafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
